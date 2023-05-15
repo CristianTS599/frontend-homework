6e8971ca0d9b44829dcaa29bebf8803ab78a6788
Cristian Torres Salazar <tor8@pdx.edu>
2023-05-15 06:29:15 -0700

    Enhancements to ex3

--- hw3/03-react/03-react-project/src/components/character.jsx
+++ hw3/03-react/03-react-project/src/components/character.jsx
@@ -1,17 +1,17 @@
-
-
 export default function Character(character) {
-    let alt = `Photo of Game of Thrones Character ${character.fullName}`;
-    
-    console.log('props');
-    console.log(character);
-return (
-    <div class="card" style="width: 18rem;">
-        <img src={character.imageUrl} class="card-img-top" alt={alt}/>
-        <div class="card-body">
-            <h5 class="card-title">{character.fullName}</h5>
-            <p class="card-text">{character.title}</p>
-        </div>
+  let alt = `Photo of Game of Thrones Character ${character.character.fullName}`;
+
+  return (
+    <div className="card">
+      <img
+        src={character.character.imageUrl}
+        className="card-img-top"
+        alt={alt}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{character.character.fullName}</h5>
+        <p className="card-text">{character.character.title}</p>
+      </div>
     </div>
-    );
+  );
 }
